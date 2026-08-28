1411ca5c61ab91d7169bb65ad55f3b16cfdc78fc
Tiziano Arduino Brunelli Della Mea <tiziano.brunelli@mi.unc.edu.ar>
2026-08-28 23:10:44 +0000

    actualizo practico2.py

--- Numerico_II/practicos/practico2.py
+++ Numerico_II/practicos/practico2.py
@@ -39,7 +39,7 @@ def dlu(A):
     L=np.eye(N)
     for i in range(N):
         assert(abs(U[i,i])>EPS)
-        L[i+1:,i]=U[i+1:,i]/U[i][i]
+        L[i+1:,i]=U[i+1:,i]/U[i,i]
         U[i+1:,i]=np.zeros((1,N-i-1),dtype=np.float64)
         U[i+1:,i+1:]-=np.outer(L[i+1:,i],U[i,i+1:])
     return (L,U)
@@ -77,7 +77,124 @@ def egauss_window3(_A,_b):
 
 print(egauss_window3(A,b),"egauss window")
 
-A=np.array([[1,5,7],[2,8,3],[1,1,1]],dtype=np.float64)
-b=np.array([1,2,3],dtype=np.float64)
-
 print(egauss_window3(B,c),"egaus window")
+
+#ej 10)
+
+def egaussp(_A,_b):
+    (N,M)=_A.shape
+    assert(N==_b.shape[0])
+    A=np.block([np.ndarray.copy(_A),_b.reshape(-1,1)])
+    p=[i for i in range(N)]
+    for i in range(N):
+        for j in range(i+1,N):
+            if(abs(A[p[j],i])>abs(A[p[i],i])): p[i],p[j] = p[j],p[i]
+        if(abs(A[p[i],i])<=EPS): continue
+        A[p[i],i+1:]/=A[p[i],i]
+        A[p[i],i]=1
+        for j in range(i+1,N):
+            A[p[j],i+1:]-=A[p[i],i+1:]*A[p[j],i]
+            A[p[j],i]=0
+    return (A,p)
+
+def dlup(A):
+    (N,M)=A.shape
+    assert(N==M)
+    U=np.ndarray.copy(A)
+    L=np.zeros((N,N))
+    p=[i for i in range(N)]
+    for i in range(N):
+        for j in range(i+1,N):
+            if(abs(U[p[j],i])>abs(U[p[i],i])): p[i],p[j] = p[j],p[i]
+        L[p[i],i]=1
+        for j in range(i+1,N):
+            L[p[j],i]=U[p[j],i]/U[p[i],i]
+            U[p[j],i]=0
+            U[p[j],i+1:]-=U[p[i],i+1:]*L[p[j],i]
+    return (L,U,p)
+
+(R,P)=egaussp(A,b)
+print(R,"egausp R de A")
+print(P,"egausp P de A")
+
+(R,P)=egaussp(B,c)
+print(R,"egausp R de B")
+print(P,"egausp P de B")
+
+(L,U,P)=dlup(A)
+print(L,"dlup L de A")
+print(U,"dlup U de A")
+print(P,"dlup P de A")
+
+(L,U,P)=dlup(B)
+print(L,"dlup L de B")
+print(U,"dlup U de B")
+print(P,"dlup P de B")
+
+#ej 11)
+
+def invalid(N):
+    return np.array([np.nan]*n)
+
+def sol_trinffilp(A,b,p):
+    (N,M)=A.shape
+    if(N!=b.shape[0]): return invalid(M)
+    x=np.array([0]*M,dtype=np.float64)
+    for i in range(M):
+        if(np.abs(A[p[i]][i])<=EPS):
+            if(np.abs(np.inner(A[p[i],:i],x[:i])-b[p[i]])>EPS):
+                return invalid(M)
+            continue
+        x[i]=(b[p[i]]-np.inner(A[p[i],:i+1],x[:i+1]))/A[p[i]][i]
+    return x
+
+def sol_trinfcolp(A,_b,p):
+    b=np.ndarray.copy(_b)
+    (N,M)=A.shape
+    if(N!=b.shape[0]): return invalid(M)
+    x=np.array([0]*M,dtype=np.float64)
+    for i in range(M):
+        if(np.abs(A[p[i]][i])<=EPS):
+            if(np.abs(b[p[i]])>EPS):
+                return invalid(M)
+            continue
+        x[i]=b[p[i]]/A[p[i],i]
+        for j in range(i+1,N)
+            b[p[j]]-=A[p[j],i]*x[i]
+    return x
+
+
+def sol_trsupfilp(A,b,p):
+    (N,M)=A.shape
+    if(N!=b.shape[0]): return invalid(M)
+    x=np.array([0]*M,dtype=np.float64)
+    for i in range(M-1,-1,-1):
+        if(np.abs(A[p[i]][i])<=EPS):
+            if(np.abs(np.inner(A[p[i],i+1:],x[i+1:])-b[p[i]])>EPS):
+                return invalid(M)
+            continue
+        x[i]=(b[p[i]]-np.inner(A[p[i],i+1:],x[i+1:]))/A[p[i],i]
+    return x
+
+def sol_trsupcolp(A,_b,p):
+    b=np.ndarray.copy(_b)
+    (N,M)=A.shape
+    if(N!=b.shape[0]): return invalid(M)
+    x=np.array([0]*M,dtype=np.float64)
+    for i in range(M-1,-1,-1):
+        if(np.abs(A[p[i],i])<=EPS):
+            if(np.abs(b[p[i]])>EPS):
+                return invalid(M)
+            continue
+        x[i]=b[p[i]]/A[p[i],i]
+        for j in range(i):
+            b[p[j]]-=A[:i,i],x[i])
+    return x
+
+
+
+
+def sol_egauss(_A,_b):
+    (R,P)=egaussp(_A,_b)
+    A=R[:,:R.shape[1]-1]
+    b=R[:,R.shape[1]]
